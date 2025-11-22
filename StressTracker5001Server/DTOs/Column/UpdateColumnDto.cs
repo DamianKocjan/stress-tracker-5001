3ba4906e2325c0f4eaca04bfc264bfd71518a8cc
Damian Kocjan <DamianoKPL@wp.pl>
2025-11-22 10:49:41 +0100

    fix: pr

--- StressTracker5001Server/DTOs/Column/UpdateColumnDto.cs
+++ StressTracker5001Server/DTOs/Column/UpdateColumnDto.cs
@@ -3,7 +3,6 @@ namespace StressTracker5001Server.DTOs.Column
     public class UpdateColumnDto
     {
         public required string Name { get; set; }
-        public required int Position { get; set; }
         public int? WipLimit { get; set; }
     }
 }
