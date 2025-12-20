d1d363d86fe175269938690fb948db9c2ce1e4ad
Damian Kocjan <DamianoKPL@wp.pl>
2025-12-20 12:24:13 +0100

    feat: activity log table

---
+++ StressTracker5001Server/Models/ActivityLog.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace StressTracker5001Server.Models
+{
+    public enum ActivityLogEntityType
+    {
+        Card,
+        Tag,
+        Board
+    }
+
+    public enum ActivityLogActionType
+    {
+        Created,
+        Updated,
+        Deleted,
+        Moved
+    }
+
+    public class ActivityLog
+    {
+        public int Id { get; set; }
+        public required int BoardId { get; set; }
+        public Board? Board { get; set; }
+        public required int UserId { get; set; }
+        public User? User { get; set; }
+        public required ActivityLogEntityType EntityType { get; set; }
+        public required int EntityId { get; set; }
+        public required ActivityLogActionType Action { get; set; }
+        public required string Details { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
